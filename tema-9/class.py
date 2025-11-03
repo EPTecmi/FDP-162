de79b1a9c3b76fffb97da246edc5aa21198e5d80
Edgar Patricio <edgaripatricio@tecmilenio.mx>
2025-11-03 11:43:25 -0600

    added class tema 9

---
+++ tema-9/class.py
@@ -0,0 +1,18 @@
+lista_compras = ["manzanas", 5, "leche", 1, "tortillas",.5]
+print(lista_compras)
+
+lista_compras[1] = 6
+
+lista_compras.append("pan")
+lista_compras.append(1)
+
+print(lista_compras)
+lista_compras.insert(4,"peras")
+lista_compras.insert(5,5)
+
+print(lista_compras)
+
+lista_compras.remove("manzanas")
+lista_compras.pop(0)
+
+print(lista_compras)
